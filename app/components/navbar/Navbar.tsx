7a67034330ca5f8d97bdaebb02da78b0f37c86aa
Graham Flaspoehler <grahamflas@gmail.com>
2025-07-10 15:57:17 +0200

    Add UserMenu component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
@@ -11,6 +12,8 @@ const Navbar = () => {
             <Logo />
 
             <Search />
+
+            <UserMenu />
           </div>
         </Container>
       </div>
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useState } from "react";
+
+import { AiOutlineMenu } from "react-icons/ai";
+
+import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
+
+const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+          onClick={() => console.log("Airbnb your home clicked")}
+        >
+          Airbnb your home
+        </div>
+
+        <div
+          className="p-4 md:py-1 md:px-2 border-[1px] flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+          onClick={toggleOpen}
+        >
+          <AiOutlineMenu />
+
+          <div className="hidden md:inline">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <MenuItem
+              label="Login"
+              onClick={() => console.log("Login clicked")}
+            />
+
+            <MenuItem
+              label="Sign up"
+              onClick={() => console.log("Sign up clicked")}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
