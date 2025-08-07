0f5427d33ed1915271d5a45e08df05dce7758d3d
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-07 12:52:50 +0200

    Implement ImageUpload for IMAGES step

---
+++ app/components/inputs/ImageUpload.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Image from "next/image";
+
+import { CldUploadWidget } from "next-cloudinary";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+  var cloudinary: any;
+}
+
+interface Props {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const ImageUpload = ({ onChange, value }: Props) => {
+  const handleUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  return (
+    <>
+      <CldUploadWidget
+        onSuccess={handleUpload}
+        uploadPreset="Airbnb Clone"
+        options={{ maxFiles: 1 }}
+      >
+        {({ open }) => {
+          return (
+            <button
+              className="relative hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 cursor-pointer"
+              onClick={() => open?.()}
+            >
+              <TbPhotoPlus size={50} />
+
+              <div className="font-semibold text-lg">Click to upload</div>
+
+              {value && (
+                <div className="absolute inset-0 w-full h-full">
+                  <Image
+                    alt="Upload"
+                    fill
+                    style={{ objectFit: "cover" }}
+                    src={value}
+                  />
+                </div>
+              )}
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default ImageUpload;
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -13,6 +13,7 @@ import useRentModal from "@/app/hooks/useRentModal";
 
 import { categories } from "../navbar/Categories";
 import dynamic from "next/dynamic";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -42,7 +43,7 @@ const RentModal = () => {
       guestCount: 1,
       roomCount: 1,
       bathroomCount: 1,
-      ImageSrc: "",
+      imageSrc: "",
       price: 1,
       title: "",
       description: "",
@@ -54,6 +55,7 @@ const RentModal = () => {
   const watchedGuestCount = watch("guestCount");
   const watchedRoomCount = watch("roomCount");
   const watchedBathroomCount = watch("bathroomCount");
+  const watchedImageSrc = watch("imageSrc");
 
   const Map = useMemo(
     () => dynamic(() => import("../Map"), { ssr: false }),
@@ -173,6 +175,22 @@ const RentModal = () => {
             setCustomValue("bathroomCount", updatedValue);
           }}
           value={watchedBathroomCount}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+
+        <ImageUpload
+          onChange={(imageUrl) => setCustomValue("imageSrc", imageUrl)}
+          value={watchedImageSrc}
         />
       </div>
     );
