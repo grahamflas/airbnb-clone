a03eebe89883d8c5f058ff742485b572ba2de8a3
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-09 11:11:52 +0200

    Update types in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import EmptyState from "./components/EmptyState";
 
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
+import { Listing } from "./generated/prisma";
+import { SafeUser } from "./types";
 
 export default async function Home() {
-  const currentUser = await getCurrentUser();
-  const listings = await getListings();
+  const currentUser: SafeUser | null = await getCurrentUser();
+  const listings: Listing[] = await getListings();
 
   if (listings.length === 0) {
     return <EmptyState showReset />;
@@ -16,7 +18,7 @@ export default async function Home() {
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {listings.map((listing) => (
+        {listings.map((listing: Listing) => (
           <ListingCard
             currentUser={currentUser}
             data={listing}
