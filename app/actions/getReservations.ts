eb91fcc36bf87fc9214c345eeb230b58b3e829c8
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-10 20:25:33 +0200

    Implement getReservations action to get reservations for a listing

---
+++ app/actions/getReservations.ts
@@ -0,0 +1,47 @@
+import prisma from "@/app/libs/prismadb"
+
+interface Params {
+  authorId?: string;
+  listingId?: string;
+  userId?: string;
+}
+
+interface Query {
+  [key: string]: string | Query;
+}
+
+export default async function getReservations({
+  authorId,
+  listingId,
+  userId,
+}: Params) {
+  try {
+    const query: Query = {};
+
+    if (authorId) {
+      query.listing = { userId: authorId}
+    }
+
+    if (listingId) {
+      query.listingId = listingId;
+    }
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      }
+    });
+
+    return reservations
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,6 +3,7 @@ import ListingShow from "./ListingShow";
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
+import getReservations from "@/app/actions/getReservations";
 
 interface Params {
   listingId?: string;
@@ -11,12 +12,19 @@ interface Params {
 const ListingPage = async ({ params }: { params: Params }) => {
   const currentUser = await getCurrentUser();
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
 
   if (!listing) {
     return <EmptyState />;
   }
 
-  return <ListingShow currentUser={currentUser} listing={listing} />;
+  return (
+    <ListingShow
+      currentUser={currentUser}
+      listing={listing}
+      reservations={reservations}
+    />
+  );
 };
 
 export default ListingPage;
