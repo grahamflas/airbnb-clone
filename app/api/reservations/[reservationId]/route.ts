a31652a62fa690a45c5511474e68a81f02c869fa
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-11 10:17:51 +0200

    Add endpoint to destroy a reservation

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+interface Params {
+  reservationId?: string;
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Params},
+){
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = await params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid reservationId");
+  }
+
+  // [TODO]: test to see what happens if I try to delete a reservation that isn't mine
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [
+        { userId: currentUser.id},
+        // This enables the owner of the listing for which the
+        // reservation was created (i.e., the Host) to also cancel
+        // the reservation.
+        { listing: { userId: currentUser.id } },
+      ]
+    }
+  });
+
+  return NextResponse.json(reservation);
+}
