e3a63c596d2c179a09b5333fbdff556954e5ad8d
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-14 15:06:57 +0200

    Add error

---
+++ app/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+interface Props {
+  error: Error;
+}
+
+const ErrorState = ({ error }: Props) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return <EmptyState title="Uh oh" subtitle="Something when wrong" />;
+};
+
+export default ErrorState;
