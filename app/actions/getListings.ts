176cd2f5d070313d96a3cba6fe77014363e15e00
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-14 14:29:40 +0200

    Update getListings to build query based on search params

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,14 +2,76 @@ import prisma from "@/app/libs/prismadb"
 import { Query } from "./getReservations";
 
 export interface IListingsParams {
+  bathroomCount?: number;
+  category?: string;
+  endDate?: string;
+  guestCount?: number;
+  locationValue?: string;
+  roomCount?: number;
+  startDate?: string;
   userId?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = await params;
+    const {
+      bathroomCount,
+      category,
+      endDate,
+      guestCount,
+      locationValue,
+      roomCount,
+      startDate,
+      userId,
+     } = await params;
 
     const query: Query = {};
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      }
+    };
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      }
+    };
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    };
+
+    if (roomCount) {
+      query.roomCount = {
+        // transforms roomCount into a number
+        gte: +roomCount,
+      }
+    };
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate}
+              },
+              {
+                endDate: { lte: endDate },
+                startDate: { gte: endDate}
+              },
+            ]
+          }
+        }
+      }
+    };
 
     if (userId) {
       query.userId = userId;
